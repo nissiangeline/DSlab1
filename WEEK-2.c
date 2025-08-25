60a236665c3926110c4be1949868a00426cbeefa
nissiangeline <prathibhadevarakonda08@gmail.com>
2025-08-25 02:28:55 -0700

    Add files via upload

---
+++ WEEK-2.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct queue {
+    struct node *front, *rear;
+};
+
+void create_queue(struct queue *q) {
+    q->front = q->rear = NULL;
+}
+
+void insert(struct queue *q, int val) {
+    struct node *ptr = malloc(sizeof(struct node));
+    if (ptr == NULL) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    ptr->data = val;
+    ptr->next = NULL;
+
+    if (q->rear == NULL)
+        q->front = q->rear = ptr;
+    else {
+        q->rear->next = ptr;
+        q->rear = ptr;
+    }
+}
+
+
+
+void delete_element(struct queue *q) {
+    if (q->front == NULL) {
+        printf("\nUNDERFLOW");
+        return;
+    }
+    struct node *temp = q->front;
+    printf("\nDELETED:%d", temp->data);
+    q->front = q->front->next;
+    if (q->front == NULL)
+        q->rear = NULL;
+    free(temp);
+}
+
+int peek(struct queue *q) {
+    return (q->front) ? q->front->data : -1;
+}
+
+void display(struct queue *q) {
+    if (q->front == NULL) {
+        printf("\nQUEUE IS EMPTY");
+        return;
+    }
+    struct node *temp = q->front;
+    printf("\nQUEUE: ");
+    while (temp) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    struct queue q;
+    int val, option;
+    create_queue(&q);
+    do {
+        printf("\n1. INSERT\n2. DELETE\n3. PEEK\n4. DISPLAY\n5. EXIT\n");
+        scanf("%d", &option);
+        switch (option) {
+            case 1:
+                printf("enter value: ");
+                scanf("%d", &val);
+                insert(&q, val);
+                break;
+            case 2:
+                delete_element(&q);
+                break;
+            case 3:
+                val = peek(&q);
+                if (val != -1)
+                    printf("\nfront: %d", val);
+                else
+                    printf("\nQueue IS EMPTY");
+                break;
+            case 4:
+                display(&q);
+                break;
+        }
+    } while (option != 5);
+    return 0;
+}
+
+
